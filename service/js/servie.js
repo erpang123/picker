934de73433ef9b23aabae474949856ec89843155
erpang123 <1326754391@qq.com>
2019-06-26 16:51:42 +0800

    service worker路径需要绝对路径

--- service/js/servie.js
+++ service/js/servie.js
@@ -1,5 +1,5 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/service/js/sw.js').then((registration) => {
+    navigator.serviceWorker.register('/picker/service/js/sw.js').then((registration) => {
         console.log(registration)
     }).catch(err => {
         console.log(err)
